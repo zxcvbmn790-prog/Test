11d524d56c5c8958e51c0f21905e72b4713d29e5
seoil <seoil@BY211-G-15>
2026-02-03 15:18:45 +0900

    Test

---
+++ src/SelectAll2.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SelectAll2 {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@172.16.15.67:1521:xe", "system", "1234");
+			System.out.println(conn);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
