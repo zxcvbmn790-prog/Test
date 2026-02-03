b44fe105040b57aa25c734b7edfdd8489f041d34
seoil <seoil@BY211-G-15>
2026-02-03 15:21:34 +0900

    Test2

--- src/SelectAll2.java
+++ src/SelectAll2.java
@@ -8,6 +8,7 @@ public class SelectAll2 {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@172.16.15.67:1521:xe", "system", "1234");
+			System.out.println("qweqweqweqweqwe");
 			System.out.println(conn);
 		} catch (SQLException e) {
 			e.printStackTrace();
